2be73b7f66ecce8d9a175758acaa7ced5002d938
Shawn Chen <meishang.chen@shopify.com>
2022-12-01 10:58:32 -0330

    modify day 1 solution

--- day1/dayOne.js
+++ day1/dayOne.js
@@ -1,33 +1,29 @@
 const { syncReadFile } = require("../readFiles");
-const data = syncReadFile("./day1/dayOneInput.txt");
 
 function sumElvesCalories(array) {
-  const total = [];
   let temp = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === "") {
-      total.push(temp);
-      temp = 0;
-      continue;
-    }
-    temp = temp + parseInt(array[i]);
-  }
-  return total;
-}
-
-function findMax(totalArray) {
-  const sorted = totalArray.sort((a, b) => a - b);
-  return sorted[sorted.length - 1];
+  return array
+    .reduce((total, i) => {
+      if (i === "") {
+        total.push(temp);
+        temp = 0;
+        return total;
+      }
+      temp += Number(i);
+      return total;
+    }, [])
+    .sort((a, b) => a - b);
 }
 
 function findTopXSum(totalArray, number) {
-  const sorted = totalArray.sort((a, b) => a - b);
-  const topX = sorted.slice(-1 * number);
-  return topX.reduce((a, b) => a + b);
+  return totalArray.slice(-1 * number).reduce((a, b) => a + b);
 }
+
+const data = syncReadFile("./day1/dayOneInput.txt");
 const total = sumElvesCalories(data);
-const max = findMax(total);
+const top1Sum = findTopXSum(total, 1);
 const top3Sum = findTopXSum(total, 3);
+
 console.log("total", total);
-console.log("max", max);
+console.log("top1Sum", top1Sum);
 console.log("top3Sum", top3Sum);
