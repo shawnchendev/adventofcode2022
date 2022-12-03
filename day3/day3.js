9cff13187ccc3fc842b5fe9077333275f6e2f998
Shawn Chen <meishangchen@gmail.com>
2022-12-03 13:49:50 -0330

    Day 3 solution

---
+++ day3/day3.js
@@ -0,0 +1,72 @@
+const { syncReadFile } = require("../readFiles.js");
+
+const input = syncReadFile("day3/input.txt");
+
+const sampleInput = [
+  "vJrwpWtwJgWrhcsFMMfFFhFp",
+  "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+  "PmmdzqPrVvPwwTWBwg",
+  "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+  "ttgJtRGJQctTZtZT",
+  "CrZsJsPPZsGzwwsLwLmpwMDw",
+];
+
+function splitRucksack(input) {
+  let firstComp = input.slice(0, input.length / 2);
+  let secComp = input.slice(input.length / 2);
+
+  return [firstComp, secComp];
+}
+
+function matchString([a, b]) {
+  const setA = new Set(a);
+  const setB = new Set(b);
+  const setC = [...setA].filter((x) => setB.has(x));
+  return setC[0];
+}
+
+function matchThreeString([a, b, c]) {
+  const setA = new Set(a);
+  const setB = new Set(b);
+  const setC = new Set(c);
+  const setD = [...setA].filter((x) => setB.has(x) && setC.has(x));
+  return setD[0];
+}
+
+function groupThreeInput(input) {
+  const group = [];
+  for (let i = 0; i < input.length - 2; i += 3) {
+    const subGroup = input.slice(i, i + 3);
+    group.push(subGroup);
+  }
+  return group;
+}
+
+function getMatchLetterDecimalValue(matchedLetter) {
+  return matchedLetter.charCodeAt(0) - 96 < 0
+    ? matchedLetter.charCodeAt(0) - 38 // this mean the letter is capital
+    : matchedLetter.charCodeAt(0) - 96; //else is a lower case letter
+}
+
+function findTotals(input, mapFunc) {
+  return input
+    .map((item) => {
+      return mapFunc(item);
+    })
+    .filter((item) => item)
+    .reduce((total, curr) => {
+      return total + getMatchLetterDecimalValue(curr);
+    }, 0);
+}
+
+let total = findTotals(input, (item) => {
+  const compartments = splitRucksack(item);
+  return matchString(compartments);
+});
+
+console.log(total);
+
+let partTwoTotal = findTotals(groupThreeInput(input), (item) => {
+  return matchThreeString(item);
+});
+console.log(partTwoTotal);
