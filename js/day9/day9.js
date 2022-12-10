d96dd1fd81b869ea0133607aadf4d712ed369760
Shawn Chen <meishangchen@gmail.com>
2022-12-10 11:01:42 -0330

    Day 9 solution

--- js/day9/day9.js
+++ js/day9/day9.js
@@ -1,6 +1,7 @@
 const { syncReadFile } = require("../readFiles");
 
 const sampleInputs = syncReadFile("./js/day9/sampleInput.txt");
+const sampleInputsP2 = syncReadFile("./js/day9/sampleInputP2.txt");
 const inputs = syncReadFile("./js/day9/input.txt");
 console.log(sampleInputs);
 console.log(inputs);
@@ -34,40 +35,60 @@ function parseCommand(command) {
 }
 
 function getDifferent(head, tail) {
-  const { x: hx, y: hy } = head;
-  const { x: tx, y: ty } = tail;
-
-  return { xDelta: Math.abs(hx - tx), yDelta: Math.abs(hy - ty) };
+  return { x: head.x - tail.x, y: head.y - tail.y };
 }
 
-let head = { ...start };
-let tail = { ...start };
-let visitedSpot = new Set();
-sampleInputs.forEach((command) => {
-  if (!command) return;
-  const { direction, value } = parseCommand(command);
-  for (let i = 0; i < value; i++) {
-    head = move(head, direction);
-    const getDifferentResult = getDifferent(head, tail);
-    const { xDelta, yDelta } = getDifferentResult;
-    console.log(head, tail, getDifferentResult);
-    if (xDelta >= 1 && yDelta >= 1) {
-      tail = { x: tail.x + 1, y: tail.y + 1 };
-      visitedSpot.add(`${tail.x},${tail.y}`);
-      continue;
+function moveRope(ropeLength = 2) {
+  let rope = new Array(ropeLength).fill({ ...start });
+  let tailsVisitedSpot = new Set();
+  inputs.forEach((command) => {
+    if (!command) return;
+    const { direction, value } = parseCommand(command);
+    for (let i = 0; i < value; i++) {
+      rope[0] = move(rope[0], direction);
+      for (let j = 1; j < rope.length; j++) {
+        let head = rope[j - 1];
+        const different = getDifferent(head, rope[j]);
+        if (different.y >= 2) {
+          rope[j] = move(rope[j], "U");
+          if (different.x >= 1) {
+            rope[j] = move(rope[j], "R");
+          } else if (different.x <= -1) {
+            rope[j] = move(rope[j], "L");
+          }
+        } else if (different.y <= -2) {
+          rope[j] = move(rope[j], "D");
+          if (different.x >= 1) {
+            rope[j] = move(rope[j], "R");
+          } else if (different.x <= -1) {
+            rope[j] = move(rope[j], "L");
+          }
+        } else if (different.x >= 2) {
+          rope[j] = move(rope[j], "R");
+          if (different.y >= 1) {
+            rope[j] = move(rope[j], "U");
+          } else if (different.y <= -1) {
+            rope[j] = move(rope[j], "D");
+          }
+        } else if (different.x <= -2) {
+          rope[j] = move(rope[j], "L");
+          if (different.y >= 1) {
+            rope[j] = move(rope[j], "U");
+          } else if (different.y <= -1) {
+            rope[j] = move(rope[j], "D");
+          }
+        }
+      }
+      tailsVisitedSpot.add(
+        `${rope[rope.length - 1].x},${rope[rope.length - 1].y}`
+      );
     }
-    if (xDelta >= 1 && yDelta === 0) {
-      tail = { x: tail.x + 1, y: tail.y };
-      visitedSpot.add(`${tail.x},${tail.y}`);
-      continue;
-    }
-    if (xDelta === 0 && yDelta >= 1) {
-      tail = { x: tail.x, y: tail.y + 1 };
-      visitedSpot.add(`${tail.x},${tail.y}`);
-      continue;
-    }
-  }
-  console.log(visitedSpot);
-});
+  });
+  return tailsVisitedSpot;
+}
 
-console.log([...visitedSpot].length);
+let tailsVisitedSpotP1 = moveRope(2);
+let tailsVisitedSpotP2 = moveRope(10);
+
+console.log("part 1", [...tailsVisitedSpotP1].length);
+console.log("part 2", [...tailsVisitedSpotP2].length);
